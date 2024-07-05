8fe30f498d7a851f7b27ea5af52d6e422783eaad
Ilhom <ilhom@example.com>
2024-07-05 08:08:18 +0500

    homework

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import {Route, Routes} from "react-router-dom";
 import Home from "./routes/home/Home.jsx";
 import Users from "./routes/home/users/Users.jsx";
 import DismissalUser from "./routes/home/dismissal-user/DismissalUser.jsx";
+import Sidebar from "./components/sidebar/Sidebar.jsx";
 
 
 function App() {
 
   return (
     <div className="container">
+        <Sidebar/>
       <Routes>
-        <Route path="" element={<Home/>}>
+          <Route path="" element={<Home/>} />
           <Route path="users" element={<Users/>}/>
           <Route path="fired-user" element={<DismissalUser/>}/>
-        </Route>
       </Routes>
     </div>
   )
